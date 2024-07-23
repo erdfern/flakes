823c4acb0d261bb3cafdbe3d332de0d863dbd6e7
erdfern <rexsomnia@pm.me>
2024-07-23 18:45:23 +0200

    --wip--

---
+++ home/programs/terminals/foot.nix
@@ -0,0 +1,17 @@
+{
+  programs.foot = {
+    enable = true;
+    catppuccin.enable = true;
+
+    settings = {
+      main = {
+        term = "xterm-256color";
+        font = "Fira Code:size=14";
+        dpi-aware = "yes";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+    };
+  };
+}
