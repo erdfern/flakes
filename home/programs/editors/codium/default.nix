93ea6e6f827ee4511ccaef83ebdc3f8d19f74b46
erdfern <rexsomnia@pm.me>
2024-08-01 15:56:09 +0200

    --wip--

---
+++ home/programs/editors/codium/default.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+      ms-python.python
+    ];
+  };
+}
