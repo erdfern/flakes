71af562064be09ee1407c3a341480ac5ce0c214d
erdfern <rexsomnia@pm.me>
2024-09-26 14:31:41 +0200

    --wip--

--- home/programs/messengers/default.nix
+++ home/programs/messengers/default.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
-  # home.packages = (with pkgs; [
+ home.packages = (with pkgs; [
+    signal-desktop
     # tdesktop
-    # signal-desktop
     # whatsapp-for-linux
-  # ]);
+  ]);
 }
