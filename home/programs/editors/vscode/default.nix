df48cdd935bad28bc008ab22c6aab7a9a0fec0c0
erdfern <rexsomnia@pm.me>
2024-09-30 15:01:36 +0200

    --wip--

--- home/programs/editors/vscode/default.nix
+++ home/programs/editors/vscode/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.vscode = {
-    enable = true;
+    enable = false;
     # programs.vscode.package = pkgs.vscode.fhsWithPackages (ps: with ps; [ rustup zlib openssl.dev pkg-config ]);
     package = pkgs.vscode.fhs;
     extensions = with pkgs.vscode-extensions; [
