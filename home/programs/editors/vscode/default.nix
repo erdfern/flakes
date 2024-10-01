37e8223d36d528fe16c4be8790dad3501233bf4f
erdfern <rexsomnia@pm.me>
2024-10-01 12:15:10 +0200

    --wip--

--- home/programs/editors/vscode/default.nix
+++ home/programs/editors/vscode/default.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 {
   programs.vscode = {
-    enable = false;
+    enable = true;
     # programs.vscode.package = pkgs.vscode.fhsWithPackages (ps: with ps; [ rustup zlib openssl.dev pkg-config ]);
     package = pkgs.vscode.fhs;
     extensions = with pkgs.vscode-extensions; [
+      ms-vscode-remote.remote-containers
       vscodevim.vim
-      yzhang.markdown-all-in-one
+      # yzhang.markdown-all-in-one
       ms-python.python
-      ms-toolsai.jupyter
+      # ms-toolsai.jupyter
     ];
   };
 }
