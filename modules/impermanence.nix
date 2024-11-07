72e626ef4926bb29b9927f7d3c151fe6a48d17d5
erdfern <rexsomnia@pm.me>
2024-11-07 19:31:19 +0100

    --wip--

--- modules/impermanence.nix
+++ modules/impermanence.nix
@@ -9,10 +9,11 @@
         "/var/lib"
         "/etc/secureboot"
       ];
-      # files = [
-      #   "/etc/machine-id"
+      files = [
+      # TODO: fix
+        "/etc/machine-id"
       #   "/etc/create_ap.conf"
-      # ];
+      ];
       users.${user} = {
         directories = [
           "Downloads"
