0c0a6408c6891754e418951731d4bb43ea092416
erdfern <rexsomnia@pm.me>
2024-11-29 11:57:13 +0100

    --wip--

--- home/desktop/wm/hyprland/hyprpaper/default.nix
+++ home/desktop/wm/hyprland/hyprpaper/default.nix
@@ -4,14 +4,14 @@ let
     url = "https://i.redd.it/mvev8aelh7zc1.png";
     hash = "sha256-lJjIq+3140a5OkNy/FAEOCoCcvQqOi73GWJGwR2zT9w";
   };
-  wp = ./whale.jpg;
+  # wp = ./whale.jpg;
   # catppuccin-wall = builtins.path {
   #   path = ./wallcat.png;
   #   name = "wallcat";
   # };
 in
 {
-  services.hyprpaper= {
+  services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
@@ -19,7 +19,9 @@ in
       splash_offset = "2.0";
       preload = [ (builtins.toString wallpaper) ];
       # wallpaper = [ ",${builtins.toString wallpaper}" ];
-      wallpaper = [ wp ];
+      wallpaper = [ "$HOME/.config/whale.jpg" ];
     };
   };
+
+  home.file."whale.jpg".source = ./whale.jpg;
 }
