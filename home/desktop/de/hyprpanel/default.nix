fe35f04db6fb8a6f857766d6e87bd2d1a928d163
erdfern <rexsomnia@pm.me>
2024-12-28 17:15:26 +0100

    --wip--

--- home/desktop/de/hyprpanel/default.nix
+++ home/desktop/de/hyprpanel/default.nix
@@ -6,7 +6,7 @@
 
     # Enable the module.
     # Default: false
-    enable = true;
+    enable = false;
 
     # Automatically restart HyprPanel with systemd.
     # Useful when updating your config so that you
