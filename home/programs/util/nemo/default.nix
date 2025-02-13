d732a19284a34196de31052d577d05cb7cb9723e
erdfern <rexsomnia@pm.me>
2025-02-13 11:45:45 +0100

    --wip--

--- home/programs/util/nemo/default.nix
+++ home/programs/util/nemo/default.nix
@@ -1,5 +1,20 @@
 { pkgs, ... }:
 {
   home.packages = (with pkgs; [ nemo-with-extensions ]);
-  # home.packages = (with pkgs; [ xfce.thunar lxqt.pcmanfm-qt]);
+  xdg.desktopEntries.nemo = {
+    name = "Nemo";
+    exec = "${pkgs.nemo-with-extensions}/bin/nemo";
+  };
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "inode/directory" = [ "nemo.desktop" ];
+      "application/x-gnome-saved-search" = [ "nemo.desktop" ];
+    };
+    dconf = {
+      settings = {
+        "org/cinammon/desktop/applications/terminal" = { exec = "kitty"; };
+      };
+    };
+  };
 }
